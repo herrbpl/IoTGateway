5a20b7bcbef3241395a84785ca8d3440dad37b4e
Siim Aus <siim@sotf.ee>
2018-06-25 14:23:48 +0300

    Add project files.

---
+++ DeviceReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeviceReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
