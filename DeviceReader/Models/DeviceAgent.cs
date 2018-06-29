5fabdc958389c029db5edcc81938786e5495ef06
Siim Aus <siim@sotf.ee>
2018-06-29 19:50:19 +0300

    Gah, this nested tasking gives me headache..

--- DeviceReader/Models/DeviceAgent.cs
+++ DeviceReader/Models/DeviceAgent.cs
@@ -7,36 +7,33 @@ using DeviceReader.Diagnostics;
 
 namespace DeviceReader.Models
 {
-    public enum DeviceStatus
-    {
-        None = 1,
-        Stopped = 2,       
-        Running = 3
-        
-    }
+
 
     public interface IDeviceAgent
     {
-        DeviceStatus OperationalStatus { get; }
+        bool IsRunning { get; }
         Task RunAsync();
         void Stop();
     }
     class DeviceAgent : IDeviceAgent
     {
-        public DeviceStatus OperationalStatus => _operationalStatus;
+        public bool IsRunning { get { return (_executingTask == null? false:_executingTask.IsCompleted); } }
+
         public CancellationTokenSource CancellationTokenSource { get => this._cts;  } 
 
-        private DeviceStatus _operationalStatus;
+
         private CancellationTokenSource _cts;
         private ILogger _logger;
         private Device _device;
+        private Task _executingTask;
+
         private Func<DeviceAgent, Task> _runDelegate;
 
         public DeviceAgent(ILogger logger, Device device, Func<DeviceAgent, Task> runDelegate)
         {
             this._cts = new CancellationTokenSource();
             this._logger = logger;
-            this._operationalStatus = DeviceStatus.None;            
+             
             this._device = device;
             if (runDelegate != null)
             {
@@ -44,35 +41,26 @@ namespace DeviceReader.Models
             } else
             {
                 this._runDelegate = DefaultRunner;
-            }
-            
+            }            
         }
+
+        
 
         public DeviceAgent(ILogger logger, Device device)        
             : this(logger, device, null)
         {
 
         }
-        
+                
         protected async Task DefaultRunner(DeviceAgent agent)
         {
-            // should we start custom iot hub output runner here?
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-            Task.Run(async () =>
-             {
-                 while(!agent.CancellationTokenSource.Token.IsCancellationRequested)
-                 {
-                     Console.WriteLine("This is just an buzz thread!");
-                     await Task.Delay(5000, agent.CancellationTokenSource.Token);
-                 }
-
-             }, agent.CancellationTokenSource.Token);
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+           
             while (!agent.CancellationTokenSource.IsCancellationRequested)
             {
                 try
                 {
-                    // Check config change
+                    // Check config change.. or should we? Perhaps.. Stop and recreate?
+                    // source can change. Target can change. 
                     // execute protocol reader
                     // execute parser/formatter
                     // execute filter
@@ -87,24 +75,56 @@ namespace DeviceReader.Models
         }
 
         public async Task RunAsync()
-        {                        
-            if (this._runDelegate != null)
+        {  
+            
+            if (this.IsRunning) { return;  }
+
+            _logger.Info(string.Format("Starting device {0}", this._device.Id), () => { });
+
+            
+            Task t = new Task( () =>
             {
-                if (this._operationalStatus == DeviceStatus.None || this._operationalStatus == DeviceStatus.Stopped)
+
+                while (!this.CancellationTokenSource.IsCancellationRequested)
                 {
-                    _logger.Info(string.Format("Starting device {0}", this._device.Id), () => { });
-                    this._operationalStatus = DeviceStatus.Running;
-                    await this._runDelegate(this);                    
+                    try
+                    {
+                        // Check config change.. or should we? Perhaps.. Stop and recreate?
+                        // source can change. Target can change. 
+                        // execute protocol reader
+                        // execute parser/formatter
+                        // execute filter
+                        // post to output/iot Hub
+                        _logger.Info("Runner thread loop", () => { });
+                         Task.Delay(1000, this.CancellationTokenSource.Token).Wait();
+                    }
+                    catch (OperationCanceledException ex)
+                    {
+                        _logger.Info("Cancellation requested!", () => { });
+                    }
                 }
+
+            });
+
+            try
+            {
+                this._executingTask = t;
+                t.Start();
+                await this._executingTask; // this._runDelegate(this);                    
             }
+            catch (AggregateException ex)
+            {
+                _logger.Info(string.Format("Error: {0}", ex.InnerException), () => { });
+            }
+            //await this._runDelegate(this); // this._runDelegate(this);                    
 
         }
 
         public void Stop()
-        {
-            _logger.Info(string.Format("Stopping device {0}", this._device.Id), () => { });
-            this._operationalStatus = DeviceStatus.Stopped;
-            this._cts.Cancel();            
+        {            
+                _logger.Info(string.Format("Stopping device {0}", this._device.Id), () => { });
+                this._cts.Cancel();            
+            
         }
     }
 }
