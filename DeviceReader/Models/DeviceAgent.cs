85e22c6ec5242067a37cb74207b15549ee063f6f
Siim Aus <siim@sotf.ee>
2018-06-29 17:49:20 +0300

    Testing independent loop

--- DeviceReader/Models/DeviceAgent.cs
+++ DeviceReader/Models/DeviceAgent.cs
@@ -44,8 +44,6 @@ namespace DeviceReader.Models
             } else
             {
                 this._runDelegate = DefaultRunner;
-
-
             }
             
         }
@@ -58,6 +56,18 @@ namespace DeviceReader.Models
         
         protected async Task DefaultRunner(DeviceAgent agent)
         {
+            // should we start custom iot hub output runner here?
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            Task.Run(async () =>
+             {
+                 while(!agent.CancellationTokenSource.Token.IsCancellationRequested)
+                 {
+                     Console.WriteLine("This is just an buzz thread!");
+                     await Task.Delay(5000, agent.CancellationTokenSource.Token);
+                 }
+
+             }, agent.CancellationTokenSource.Token);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             while (!agent.CancellationTokenSource.IsCancellationRequested)
             {
                 try
@@ -67,6 +77,7 @@ namespace DeviceReader.Models
                     // execute parser/formatter
                     // execute filter
                     // post to output/iot Hub
+                    _logger.Info("Runner thread loop", () => { });
                     await Task.Delay(1000, agent.CancellationTokenSource.Token);
                 } catch (OperationCanceledException ex)
                 {
