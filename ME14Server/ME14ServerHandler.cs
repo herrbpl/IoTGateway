6f474483ecb8b860cafafa32e17e58fced093a3f
Siim Aus <siim@sotf.ee>
2018-09-06 16:46:08 +0300

    Added ME14 server emulator

---
+++ ME14Server/ME14ServerHandler.cs
@@ -0,0 +1,185 @@
+﻿namespace ME14Server
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Net;
+    using System.Text;
+    using System.Threading.Tasks;
+    using DotNetty.Transport.Channels;
+    
+
+    public class ME14ServerHandler : SimpleChannelInboundHandler<string>
+    {
+        static string servername = "me14emulator";
+        bool linestatus = false;
+        Dictionary<int, string> lastObservations;
+        public static string Identity
+        {
+            get
+            {
+                string date = DateTime.Now.ToString("yyyy-MM-dd  HH:mm");
+                return $"{date} 01 {servername}";
+            }
+        }
+
+        static string AsFieldString(string code, string value)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (value.Length > 6) throw new ArgumentException($"Argument length too big '{code}:{value.Length}'");
+            sb.Append(code).Append(value.PadLeft(6, ' '));
+            return sb.ToString();
+        }
+
+        static Dictionary<int,string> RandomObservations()
+        {
+            Dictionary<int, string> ro = new Dictionary<int, string>();
+            Random r = new Random();
+            
+            for (int i = 0; i < 40; i++)
+            {
+                double value = r.Next(1, 100) + r.NextDouble();
+                if (r.Next(0, 10) == 0) { value = -value; }
+                ro.Add(i, value.ToString("F", CultureInfo.InvariantCulture));
+            }
+
+            return ro;
+        }
+
+        static string GenerateME14Message(bool history, Dictionary<int,string> observations)
+        {
+            string date = DateTime.Now.ToString("yyyy-MM-dd  HH:mm");
+            string mtype = (history ? "H" : "M");
+            string header = $"{date},01,{mtype}14,{servername}";
+
+
+            StringBuilder obs = new StringBuilder();
+            List<int> keys = observations.Keys.ToList();
+            keys.Sort();
+
+            int column = 1;
+            foreach (var item in keys)
+            {
+                if (column == 8)
+                {
+                    obs.Append("\r\n");
+                    column = 1;
+                }
+                obs.Append(AsFieldString(item.ToString().PadLeft(2, '0'), observations[item])).Append(";");
+                column++;
+            }
+
+            var obsbytes = Encoding.ASCII.GetBytes(obs.ToString());
+            int chksum = 0;
+            for (int i = 0; i < obsbytes.Length; i++)
+            {
+                chksum += obsbytes[i];
+            }
+
+            var CheckSum = chksum.ToString("X").ToUpper();
+
+            obs.Append("\r\n=");
+            string message = "";
+
+            if (history)
+            {
+                message = $"\r\n#\r\n\r\n{header}\r\n{obs.ToString()}\r\n\r\n{CheckSum}\r\n\r\n#\r\n=";
+            } else
+            {
+                message = $"MES14\r\n\r\n{header}\r\n{obs.ToString()}\r\n{CheckSum}\r\n";
+            }
+
+            return message;
+            
+        }
+
+       /* public override void ChannelActive(IChannelHandlerContext contex)
+        {
+            contex.WriteAsync(string.Format("Welcome to {0} !\r\n", Dns.GetHostName()));
+            contex.WriteAndFlushAsync(string.Format("It is {0} now !\r\n", DateTime.Now));
+        }
+        */
+        protected override void ChannelRead0(IChannelHandlerContext contex, string msg)
+        {
+            // Generate and write a response.
+            string response;
+            bool close = false;
+            if (string.IsNullOrEmpty(msg))
+            {
+                return;
+            }
+            else if (string.Equals("OPEN 1", msg, StringComparison.OrdinalIgnoreCase))
+            {
+                if (!linestatus)
+                {
+                    response = $"{Identity}\r\n\r\nLINE A OPENED\r\n\r\n>";
+                    linestatus = true;
+                }  else
+                {
+                    return;
+                }
+            }
+            else if (string.Equals("CLOSE", msg, StringComparison.OrdinalIgnoreCase))
+            {
+                if (linestatus)
+                {
+                    response = $"\r\n{Identity}\r\n\r\nLINE A CLOSED";
+                    linestatus = false;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else if (string.Equals("ME14", msg, StringComparison.OrdinalIgnoreCase))
+            {
+                if (linestatus)
+                {
+                    lastObservations = RandomObservations();
+                    response = GenerateME14Message(false, lastObservations)+"\r\n>";                    
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else if (string.Equals("HIST", msg, StringComparison.OrdinalIgnoreCase))
+            {
+                if (linestatus)
+                {
+                    if (lastObservations == null) lastObservations = RandomObservations();
+                    response = GenerateME14Message(true, lastObservations) + "\r\n>";
+                }
+                else
+                {
+                    return;
+                }
+            } else
+            {
+                return;
+            }
+            
+
+            Task wait_close = contex.WriteAndFlushAsync(response);
+            if (close)
+            {
+                Task.WaitAll(wait_close);
+                contex.CloseAsync();
+            }
+        }
+
+        public override void ChannelReadComplete(IChannelHandlerContext contex)
+        {
+            contex.Flush();
+        }
+
+        public override void ExceptionCaught(IChannelHandlerContext contex, Exception e)
+        {
+            Console.WriteLine("{0}", e.StackTrace);
+            contex.CloseAsync();
+        }
+
+        public override bool IsSharable => true;
+    }
+}
---
+++ ME14Server/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+namespace ME14Server
+{
+    using System;
+    using System.IO;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Threading.Tasks;
+    using DotNetty.Codecs;
+    using DotNetty.Handlers.Logging;
+    using DotNetty.Handlers.Tls;
+    using DotNetty.Transport.Bootstrapping;
+    using DotNetty.Transport.Channels;
+    using DotNetty.Transport.Channels.Sockets;
+    using DotNetty.Common.Internal.Logging;
+    using Microsoft.Extensions.Logging.Console;
+    using CommandLine;
+
+    class Program
+    {
+        class Options
+        {
+
+            [Option(Required = false, HelpText = "Server port to use, 5000 default", Default = 5000)]
+            public int serverport { get; set; }
+
+            [Option(Required = false, HelpText = "Use SSL for authentication")]
+            public bool usessl { get; set; }
+
+            [Option(Required = false, HelpText = "PFX File Path")]
+            public string pfxfile { get; set; }
+
+            [Option(Required = false, HelpText = "PFX File password")]
+            public string pfxpassword { get; set; }
+        }
+
+        public static string ProcessDirectory
+        {
+            get
+            {
+#if NETSTANDARD1_3
+                return AppContext.BaseDirectory;
+#else
+                return AppDomain.CurrentDomain.BaseDirectory;
+#endif
+            }
+        }
+
+
+        static async Task RunServerAsync(string[] args)
+        {
+
+            int serverport = 5000;
+            string path = null;
+            bool exitfromopts = false;
+            X509Certificate2 tlsCertificate = null;
+
+            Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(opts =>
+            {
+                
+
+                if (opts.usessl)
+                {
+                    if (opts.pfxfile == null) throw new ArgumentException($"PFX File path not specified for SSL option");
+                    if (opts.pfxpassword == null) throw new ArgumentException($"PFX password not specified for SSL option");
+
+                    path = opts.pfxfile;
+
+                    if (!File.Exists(path))
+                    {
+                        path = Path.Combine(Program.ProcessDirectory, opts.pfxfile);
+                        if (!File.Exists(path))
+                        {
+                            throw new ArgumentException($"File not found: '{path}'");
+                        }
+                    }
+
+                    tlsCertificate = new X509Certificate2(path, opts.pfxpassword);
+                }
+                serverport = opts.serverport;
+
+            }).WithNotParsed<Options>((errors) => {
+                Console.WriteLine("Invalid program arguments:");
+                foreach (var err in errors)
+                {
+                    Console.WriteLine(err.ToString());
+                }
+                exitfromopts = true;
+            });
+
+            if (exitfromopts) return;
+
+
+            //ExampleHelper.SetConsoleLogger();
+            InternalLoggerFactory.DefaultFactory.AddProvider(new ConsoleLoggerProvider((s, level) => true, false));
+
+            var bossGroup = new MultithreadEventLoopGroup(1);
+            var workerGroup = new MultithreadEventLoopGroup();
+
+            var STRING_ENCODER = new StringEncoder();
+            var STRING_DECODER = new StringDecoder();
+            var SERVER_HANDLER = new ME14ServerHandler();
+                                                   
+            // run server
+
+            try
+            {
+                var bootstrap = new ServerBootstrap();
+                bootstrap
+                    .Group(bossGroup, workerGroup)
+                    .Channel<TcpServerSocketChannel>()
+                    .Option(ChannelOption.SoBacklog, 100)
+                    .Handler(new LoggingHandler(LogLevel.INFO))
+                    .ChildHandler(new ActionChannelInitializer<ISocketChannel>(channel =>
+                    {
+                        IChannelPipeline pipeline = channel.Pipeline;
+                        if (tlsCertificate != null)
+                        {
+                            pipeline.AddLast(TlsHandler.Server(tlsCertificate));
+                        }
+
+                        pipeline.AddLast(new DelimiterBasedFrameDecoder(8192, Delimiters.LineDelimiter()));
+                        pipeline.AddLast(STRING_ENCODER, STRING_DECODER, SERVER_HANDLER);
+                    }));
+
+                IChannel bootstrapChannel = await bootstrap.BindAsync(serverport);
+
+                Console.ReadLine();
+
+                await bootstrapChannel.CloseAsync();
+            }
+            finally
+            {
+                Task.WaitAll(bossGroup.ShutdownGracefullyAsync(), workerGroup.ShutdownGracefullyAsync());
+            }
+
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args) => RunServerAsync(args).Wait();
+    }
+}
