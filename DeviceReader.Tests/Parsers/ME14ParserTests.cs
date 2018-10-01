da834c2481596bb440bece1474da818ec0382ee1
Siim Aus <siim@sotf.ee>
2018-10-01 13:31:06 +0300

    Added first unit tests

---
+++ DeviceReader.Tests/Parsers/ME14ParserTests.cs
@@ -0,0 +1,55 @@
+﻿using DeviceReader.Diagnostics;
+using DeviceReader.Models;
+using DeviceReader.Parsers;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace DeviceReader.Tests.Parsers
+{
+    public class ME14ParserTests_BasicTests
+    {
+
+        public const string KEY_AGENT_EXECUTABLE_ROOT = "executables:reader";
+        public const string KEY_AGENT_FORMAT_CONFIG = KEY_AGENT_EXECUTABLE_ROOT + ":format_config";
+
+        LoggingConfig lg;
+        ILogger logger;
+
+        private readonly ITestOutputHelper output;
+
+
+        public ME14ParserTests_BasicTests(ITestOutputHelper output)
+        {
+            lg = new LoggingConfig();
+            lg.LogLevel = LogLevel.Debug;
+
+            Logger.DefaultLoggerFactory.AddProvider(new XUnitLoggerProvider(output));
+            logger = new Logger(Process.GetCurrentProcess().Id.ToString(), lg);
+            
+        }
+
+        [Fact]
+        public void FailIfNoSchemaFound()
+        {           
+            var dict = new Dictionary<string, string>
+            {
+                {KEY_AGENT_FORMAT_CONFIG, "notexistingfile.json"}                
+            };
+
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection(dict)
+                .Build();
+
+            Assert.Throws<FileNotFoundException>(() => {
+                IFormatParser<string, Observation> dummyparser = new DeviceReader.Parsers.ME14Parser(logger, KEY_AGENT_FORMAT_CONFIG, config);
+            });            
+        }
+
+    }
+}
---
+++ DeviceReader.Tests/XUnitLogger.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit.Abstractions;
+
+namespace DeviceReader.Tests
+{
+    public class XUnitLoggerProvider : ILoggerProvider
+    {
+        private ITestOutputHelper _output;
+        public XUnitLoggerProvider(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new XUnitLogger(_output);
+        }
+
+        public void Dispose()
+        {
+            return;
+        }
+    }
+
+    public class XUnitLogger : ILogger,IDisposable
+    {
+        private ITestOutputHelper _output;
+        public XUnitLogger(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return this;
+        }
+
+        public void Dispose()
+        {            
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            _output.WriteLine(formatter(state, exception));
+        }
+    }
+}
