3e7e9436a2e75c48e7d5125114fa4c2124b28d66
Siim Aus <siim@sotf.ee>
2018-10-16 15:13:47 +0300

    DevicesHelper middleware

---
+++ DeviceReader.WebService/Middleware/DevicesHelperMiddleware.cs
@@ -0,0 +1,100 @@
+﻿using DeviceReader.Devices;
+using DeviceReader.WebService.Configuration;
+using DeviceReader.WebService.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeviceReader.WebService.Middleware
+{
+    /*
+    public static class DevicesHelperMiddlewareConstants
+    {
+        public const string DeviceId = "DeviceManager-DeviceId";
+        public const string AuthenticationSchema = "DeviceManager-AuthenticationSchema";
+    }
+    */
+    // https://www.blinkingcaret.com/2017/09/13/create-your-own-asp-net-core-middleware/
+    public class DevicesHelperMiddlewareOptions
+    {
+        /// <summary>
+        /// Placeholder that indicates device id in template string
+        /// </summary>
+        public string DeviceIdKey { get; set; } = "id";
+
+        /// <summary>
+        /// List of template string to scan against.
+        /// </summary>
+        public List<string> Templates { get; set; } = new List<string>();
+    }
+
+    /// <summary>
+    /// Extracts Device ID from path and checks from device Manager, if device exists. If not, return 404. If exists, device id is stored into HttpContext and proceed with next middleware.
+    /// </summary>
+    public class DevicesHelperMiddleware
+    {
+
+        public const string DEVICEID = "DeviceManager-DeviceId";
+        public const string AUTHENTICATIONSCHEMA = "DeviceManager-AuthenticationSchema";
+
+
+        private readonly RequestDelegate _next;
+        private readonly DevicesHelperMiddlewareOptions _options;
+        public DevicesHelperMiddleware(RequestDelegate next, IOptions<DevicesHelperMiddlewareOptions> options)
+        {
+            _next = next;
+            _options = options.Value;
+        }
+       
+        public async Task Invoke(HttpContext httpContext, ILoggerFactory loggerFactory, IDeviceManager deviceManager, IAuthenticationSchemeLookup<string> authenticationSchemeLookup)
+        {
+            var logger = loggerFactory.CreateLogger(typeof(DevicesHelperMiddleware).FullName);
+
+            var routematcher = new RouteMatcher();
+
+            var id = "";
+
+            // use first match
+            foreach (var item in _options.Templates)
+            {
+                var rv = routematcher.Match(item, httpContext.Request.Path);
+                
+                if (rv.ContainsKey(_options.DeviceIdKey))
+                {
+                    id = rv[_options.DeviceIdKey].ToString();
+                    break;
+                }
+            }
+
+            // for now, just log extracted id.
+            logger.LogInformation($"Extracted id '{id}' from path '{httpContext.Request.Path}'");
+
+            
+            // if id is extracted, check if such resource exists.
+            // TODO: add custom option for checking and remove hard dependency on IDeviceManager - make middleware to a generic one?
+            if (id != "")
+            {
+                if (!deviceManager.GetDeviceListAsync().Result.Any(x => x.Id == id))
+                {
+                    logger.LogWarning($"Device with Id '{id} not found'");
+                    httpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+                    return;
+                }
+
+                // device found, add info into http context.
+                httpContext.Items.Add(DevicesHelperMiddleware.DEVICEID, id);
+                var schema = authenticationSchemeLookup.GetAuthenticationSchema(id);
+                httpContext.Items.Add(DevicesHelperMiddleware.AUTHENTICATIONSCHEMA, schema);
+
+
+            }
+
+            await _next(httpContext);
+        }
+
+    }
+}
---
+++ DeviceReader.WebService/Middleware/DevicesHelperMiddlewareExtenstions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace DeviceReader.WebService.Middleware
+{
+    
+    
+    public static class DevicesHelperMiddlewareExtenstions
+    {
+        public static IApplicationBuilder UseDevicesHelperMiddleware(this IApplicationBuilder app, DevicesHelperMiddlewareOptions options)
+        {
+            return app.UseMiddleware<DevicesHelperMiddleware>(Options.Create(options));
+        }
+
+        public static IApplicationBuilder UseDevicesHelperMiddleware(this IApplicationBuilder app, Action<DevicesHelperMiddlewareOptions> options)
+        {
+            var _options = new DevicesHelperMiddlewareOptions();
+            if (options != null)
+            {
+                options.Invoke(_options);
+            }
+
+            return app.UseMiddleware<DevicesHelperMiddleware>(Options.Create(_options));
+        }
+    }
+}
